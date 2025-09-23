b20470a437c227e2b0fd94ea918b3c14d9c3c91e
ashok9109 <aaaaashokyadav123@gmail>
2025-09-23 15:21:07 +0530

    feat: Replace Nav component with Navbar, add responsive design and navigation links
    feat: Create RecipeCard component for displaying individual recipes
    
    feat: Implement RecipeContext for managing recipe data and local storage
    
    feat: Add images for different recipe categories
    
    feat: Update main entry point to include RecipeContext and Toast notifications
    
    feat: Create About and Contact pages with navigation and content
    
    feat: Implement CreateRecipe page for adding new recipes
    
    feat: Develop Home page with category navigation and video display
    
    feat: Create PageNotFound component for handling undefined routes
    
    feat: Implement Recipes page to display a list of recipes
    
    feat: Create Singlerecipe page for viewing and editing individual recipes
    
    feat: Set up main routing structure for the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Mainroutes from "./routes/mainRoutes";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
-      app
+    <div className="py-6 px-10 w-screen h-screen overflow-x-hidden bg-white text-green-500">
+      <Navbar />
+      <Mainroutes/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default App;
